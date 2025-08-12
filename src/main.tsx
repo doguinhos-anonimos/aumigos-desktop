d63cb419ec92e034ab433c206a169e5b28ff8274
raposoG <gabriel.henrique7@hotmail.com>
2025-08-12 18:03:00 -0300

    Atualiza dependências, configurações do TypeScript e remove arquivos desnecessários. Adiciona suporte ao React Router e React Query, além de integrar o Tailwind CSS ao projeto.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import "./index.css";
+
+const router = createRouter({ routeTree });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
